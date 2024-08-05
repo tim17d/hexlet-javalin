5df615f3ca2d86b18a61c362d30d8163152574e8
Timur Zhemaldinov <tim17d92@gmail.com>
2024-08-05 21:29:31 +0300

    add query params handling

--- src/main/java/org/example/hexlet/HelloWorld.java
+++ src/main/java/org/example/hexlet/HelloWorld.java
@@ -9,7 +9,13 @@ public class HelloWorld {
             config.bundledPlugins.enableDevLogging();
         });
         // Описываем, что загрузится по адресу /
-        app.get("/", ctx -> ctx.result("Hello World"));
+        app.get("/users", ctx -> ctx.result("GET /users"));
+	    app.post("/users", ctx -> ctx.result("POST /users"));
+
+        app.get("/hello", ctx -> {
+            var greeting = "Hello, " + ctx.queryParamAsClass("name", String.class).getOrDefault("World") + "!";
+            ctx.result(greeting);
+        });
         app.start(7070); // Стартуем веб-сервер
     }
 }
